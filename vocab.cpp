51aaa225c632bb71d9556c5abe3b553bd13550a3
Adam Monahan <adammonahan93@gmail.com>
2020-07-08 15:44:30 -0700

    Deleted executables, other minor changes

--- vocab.cpp
+++ vocab.cpp
@@ -24,13 +24,11 @@
 //Words each round
 #define WORDS_PER_ROUND 10
 #define MAX_NEW_WORDS 5
+#define MIN_REVIEW_WORDS 2
 #define LEARNING_RATIO 750
 
 #define CORRECT 1
 #define INCORRECT 0
-
-
-
 
 using namespace std;
 
@@ -40,6 +38,8 @@ void strip_spaces(string& str){
 	while (isspace(str[0])) str.erase(str.begin());
 	while ((double_spaces = str.find("  ")) >= 0) str.erase(double_spaces, 1);
 }
+
+
 /*-------------------VocabWord------------------------------*/
 
 struct VocabWord{
@@ -55,10 +55,9 @@ VocabWord::VocabWord(string definition, int score){
 }
 
 void VocabWord::changeScore(bool correct){
-    if (correct){
-		if (score == 999) this->score = LEARN_UPPERBOUND;
-        this->score = min(MAX_SCORE, score + 1);
-    }else{
+	if (score == 999) this->score = LEARN_UPPERBOUND + 1;
+    if (correct) this->score = min(MAX_SCORE, score + 1);
+    else{
         if (score > LEARN_UPPERBOUND) score = LEARN_UPPERBOUND;
         this->score = max(0, score - 1);
     }
@@ -73,7 +72,6 @@ struct Category : public map<string, VocabWord*>{
 };
 
 Category::~Category(){
-    cout << "Category of size " << this->size() << " destroyed " << endl;
     for (auto it=this->begin(); it != this->end(); it++){
         delete it->second;
     } 
@@ -106,9 +104,8 @@ class VocabList : public vector<Category>{
         void printAll();
         string lookup(string&);
         bool contains(string&);
-    private:
         void addToList(string&, string&, int score = -1, int category = -1);
-        int categorize(int);
+    private:
         string getnext(string&, string*);
 };
 
@@ -152,6 +149,7 @@ string VocabList::lookup(string& word){
     }
     return "";
 }
+
 string VocabList::getnext(string& line, string* newstr){
     int delim = line.find("||");
     if (delim >= 0){
@@ -180,6 +178,7 @@ void VocabList::loadSavedList(string& filename){
 void VocabList::loadNewWords(string& filename){
     string line, word, definition;
     ifstream inputfile(filename + ".txt");
+
     if (inputfile.is_open()){
         while(getline(inputfile, line)){
             if (line.size()){
@@ -237,6 +236,11 @@ class Session{
         int getCategory(int);
         bool grade(pair<string, VocabWord*>&, string&, bool);
         bool isCorrect(string&, string&);
+        bool compareSemicolonSplit(int, string&, string&);
+        bool compareCommaSplit(int, string&, string&);
+        vector<string> tokenize(string , char);
+        vector<int> generate_random_indices(int);
+        bool is_same_list(vector<string>&,vector<string>&);
 };
 
 Session::Session(string filename){
@@ -245,12 +249,15 @@ Session::Session(string filename){
     vocablist.loadNewWords(filename);
 }
 
-void Session::save(){
-    this->vocablist.saveToFile(filename);
+void Session::round(bool reverse = false){
+    showStats();
+    fillStudyList();
+    quiz(reverse);
+    save();
 }
 
 void Session::showStats(){
-    	cout << "--------------------------------" << endl;
+    cout << "--------------------------------" << endl;
 	int hard_size = vocablist[HARD].size(); 
 	int new_size = vocablist[NEW].size();
 	int learning_size = vocablist[FAMILIAR].size();
@@ -285,134 +292,62 @@ void Session::showStats(){
 	cout << "--------------------------------" << endl;
 }
 
-void Session::categorize(string key, VocabWord* word){
-    int cat = getCategory(word->score);
-    vocablist[cat][key] = word;
-}
-
-int Session::getCategory(int score){
-    if (score <= HARD_UPPERBOUND) return HARD;
-    if (score <= LEARN_UPPERBOUND) return FAMILIAR;
-    if (score <= MAX_SCORE) return REVIEW;
-    return LEARNED;
-}
-
-vector<int> generateIndices(int num){
-    vector<int> indices(num);
-    for (int i = 0; i < num; i++) indices[i] = i;
-    shuffle(indices.begin(), indices.end(), default_random_engine(time(NULL)));
-    return indices;
-}
-
-int Session::fromCatToStudyList(int to_add, int cat){
-    Category::iterator it;
-    int added = 0;
-    vector<int> indices = generateIndices(vocablist[cat].size());
-    to_add = min(to_add, (int)vocablist[cat].size());
-    for (added = 0; added < to_add; added++){
-        auto word = vocablist[cat].at_index(indices[added]);
-        studyList[word.first] = word.second;
-    }
-    for (auto word : studyList){
-        if (vocablist[cat].count(word.first)) vocablist[cat].erase(word.first);
-    }
-    return added;
+void Session::save(){
+    this->vocablist.saveToFile(filename);
 }
 
 void Session::fillStudyList(){
     int toadd = WORDS_PER_ROUND;
     int min_familiar = pow(vocablist[FAMILIAR].size(), 2) / LEARNING_RATIO;
-printf("min familiar = %d", min_familiar);
 	toadd -= fromCatToStudyList(toadd, HARD);
     toadd -= fromCatToStudyList(min(toadd, min_familiar), FAMILIAR);
     toadd -= fromCatToStudyList(min(toadd, MAX_NEW_WORDS), NEW);
     toadd -= fromCatToStudyList(toadd, FAMILIAR);
-	fromCatToStudyList(toadd, REVIEW);
-    fromCatToStudyList(2, REVIEW);
+	fromCatToStudyList(toadd + MIN_REVIEW_WORDS, REVIEW);
 }
 
-void Session::round(bool reverse = false){
-    showStats();
-    fillStudyList();
-    quiz(reverse);
-    save();
-}
+int Session::fromCatToStudyList(int to_add, int cat){
+    pair<string, VocabWord*> word;
+    Category::iterator it;
+    int added = 0;
+    vector<int> indices = generate_random_indices(vocablist[cat].size());
 
-vector<string> tokenize(string separated, char by){
-    vector<string> ans;
-    string next_word;
-    stringstream tokens(separated);
-
-    while(getline(tokens, next_word, by)){
-        strip_spaces(next_word);
-        ans.push_back(next_word);
+    to_add = min(to_add, (int)vocablist[cat].size());
+    for (added = 0; added < to_add; added++){
+        word = vocablist[cat].at_index(indices[added]);
+        studyList[word.first] = word.second;
+    }
+    for (auto word : studyList){
+        if (vocablist[cat].count(word.first)) vocablist[cat].erase(word.first);
     }
 
-    return ans;
+    return added;
 }
 
-bool is_same_list(vector<string>& l1,vector<string>& l2){
-	if (l1.size() != l2.size()) return false;
-	stack<string> temp_list;
-    int matches = 0;
+void Session::quiz(bool reverse){
+    string response;
+    pair<string, VocabWord*> word;
+    vector<pair<string, VocabWord*> > to_delete;
+    vector<int> indices = generate_random_indices(studyList.size());
+    bool correct = false;
 
-	for (string str : l1){
-		temp_list.push(str);
-	}
-    while (temp_list.size()){
-        string str = temp_list.top();
-        temp_list.pop();
-        for (string str2 : l2){
-            if (str.compare(str2) == 0){
-                matches++;
-                break;
-            }
-        }
+    cout << endl;
+    for (int i = 0; i < indices.size(); i++){
+        word = studyList.at_index(indices[i]);
+        cout << i + 1  << ") ";
+        cout << (reverse ? word.second->definition : word.first) << ": ";
+        getline(cin, response);
+        correct = grade(word, response, reverse);
+        if (correct) to_delete.push_back(word);
     }
-
-	return matches == l1.size();
-}
-
-bool isSameSet(string set1, string set2, char delim){
-    vector<string> firstSet = tokenize(set1, delim);
-    vector<string> secondSet = tokenize(set2, delim);
-
-    return is_same_list(firstSet, secondSet);
-}
-
-bool Session::isCorrect(string& response, string& answer){
-    if (response.compare(answer) == 0) return true;
-    string also_accepted = answer;
-
-    int open_paren = answer.find("(");
-	if (open_paren > -1){
-		also_accepted.erase(open_paren, answer.find(")") - open_paren + 1);
-        strip_spaces(also_accepted);
-		if (isCorrect(response, also_accepted)){
-			cout << answer << endl;
-			return true;
-		}
-	}
-
-    int num_commas = count(response.begin(), response.end(), ',');
-	if (num_commas > 0 && num_commas == count(also_accepted.begin(), also_accepted.end(), ',')){
-        if (response.rfind("to ", 2) == 0) response.erase(0, 3);
-        if (also_accepted.rfind("to ", 2) == 0) also_accepted.erase(0, 3);
-        if (isSameSet(response, also_accepted, ',')){
-			cout << answer << endl;
-			return true;
-		}
-	}
-
-    int num_semicolons = count(response.begin(), response.end(), ';');
-	if (num_semicolons > 0 && num_semicolons == count(also_accepted.begin(), also_accepted.end(), ';')){
-        if (isSameSet(response, also_accepted, ';')){
-			cout << answer << endl;
-			return true;
-		}
-	}
-
-    return false;
+    for (pair<string, VocabWord*> word : to_delete){
+        this->categorize(word.first, word.second);
+        studyList.erase(word.first);
+    }
+    if (studyList.size()){
+        cout << studyList.size() << " to try again:" << endl;
+        quiz(reverse);
+    }
 }
 
 bool Session::grade(pair<string, VocabWord*>& word, string& response, bool reverse){
@@ -436,28 +371,93 @@ bool Session::grade(pair<string, VocabWord*>& word, string& response, bool rever
     return true;
 }
 
-void Session::quiz(bool reverse){
-    pair<string, VocabWord*> word;
-    string response;
-    vector<pair<string, VocabWord*> > to_delete;
-    vector<int> indices = generateIndices(studyList.size());
-    bool correct = false;
+bool Session::isCorrect(string& response, string& answer){
+    if (response.compare(answer) == 0) return true;
+    string also_accepted = answer;
 
-    cout << endl;
-    for (int i = 0; i < indices.size(); i++){
-        word = studyList.at_index(indices[i]);
-        cout << i + 1  << ") ";
-        cout << (reverse ? word.second->definition : word.first) << ": ";
-        getline(cin, response);
-        correct = grade(word, response, reverse);
-        if (correct) to_delete.push_back(word);
+    int open_paren = answer.find("(");
+	if (open_paren > -1){
+		also_accepted.erase(open_paren, answer.find(")") - open_paren + 1);
+        strip_spaces(also_accepted);
+		if (isCorrect(response, also_accepted)){
+			cout << answer << endl;
+			return true;
+		}
+	}
+
+    int semicolons = count(response.begin(), response.end(), ';');
+    if (semicolons)  return compareSemicolonSplit(semicolons, response, also_accepted);
+
+    int commas = count(response.begin(), response.end(), ',');
+    if (commas) return compareCommaSplit(commas, response, also_accepted);
+
+    return false;
+}
+
+bool Session::compareSemicolonSplit(int num_semicolons, string& response, string& answer){
+    vector<string> answers = tokenize(answer, ';');
+    vector<string> responses = tokenize(response, ';');
+
+    return is_same_list(answers, responses);
+}
+
+bool Session::compareCommaSplit(int num_commas, string& response, string& answer){
+    if (response.rfind("to ", 2) == 0) response.erase(0, 3);
+    if (answer.rfind("to ", 2) == 0) answer.erase(0, 3);
+    vector<string> answers = tokenize(answer, ',');
+    vector<string> responses = tokenize(response, ',');
+
+    return is_same_list(answers, responses);
+}
+
+void Session::categorize(string key, VocabWord* word){
+    int cat = getCategory(word->score);
+    vocablist[cat][key] = word;
+}
+
+int Session::getCategory(int score){
+    if (score <= HARD_UPPERBOUND) return HARD;
+    if (score <= LEARN_UPPERBOUND) return FAMILIAR;
+    if (score <= MAX_SCORE) return REVIEW;
+    return LEARNED;
+}
+
+vector<int> Session::generate_random_indices(int num){
+    vector<int> indices(num);
+    for (int i = 0; i < num; i++) indices[i] = i;
+    shuffle(indices.begin(), indices.end(), default_random_engine(time(NULL)));
+    return indices;
+}
+
+vector<string> Session::tokenize(string separated, char by){
+    vector<string> ans;
+    string next_word;
+    stringstream tokens(separated);
+
+    while(getline(tokens, next_word, by)){
+        strip_spaces(next_word);
+        ans.push_back(next_word);
     }
-    for (auto word : to_delete){
-        this->categorize(word.first, word.second);
-        studyList.erase(word.first);
+
+    return ans;
+}
+
+bool Session::is_same_list(vector<string>& l1,vector<string>& l2){
+    string str;
+	stack<string, vector<string> > temp_stack(l1);
+    int matches = 0;
+
+	if (l1.size() != l2.size()) return false;
+    while (temp_stack.size()){
+        str = temp_stack.top();
+        temp_stack.pop();
+        for (string str2 : l2){
+            if (isCorrect(str, str2)){
+                matches++;
+                break;
+            }
+        }
     }
-    if (studyList.size()){
-        cout << studyList.size() << " to try again:" << endl;
-        quiz(reverse);
-    }
+
+	return matches == l1.size();
 }
